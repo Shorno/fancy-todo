afb113d3a96906594c5ea5678b5497c6ddac931f
Shorno <fb.shorno@gmail.com>
2024-06-16 14:52:46 +0600

    adding new items via useState

--- components/main/AddItemForm.jsx
+++ components/main/AddItemForm.jsx
@@ -1,29 +1,47 @@
 "use client"
 import Button from "@/components/main/Button";
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 export default function AddItemForm({setItems}) {
     const [itemText, setItemText] = useState("")
+    const inputRef = useRef();
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        if (!itemText) {
+            alert("Items cannot be empty")
+            inputRef.current.focus();
+            return;
+        }
+        const newItem = {
+            id: new Date().getTime(),
+            name: itemText,
+            packed: false
+        }
+        setItems(prev => [...prev, newItem])
+        setItemText("");
+    }
+
+
+    useEffect(() => {
+        inputRef.current.focus();
+    }, [itemText])
+
     return (
         <>
-            <form onSubmit={(event) => {
-                event.preventDefault()
-                const newItem = {
-                    id: 4,
-                    name: itemText,
-                    packed: false
-                }
-                setItems(prev => [...prev, newItem])
-            }}
+            <form onSubmit={handleSubmit}
                   className={"w-full p-5"}>
                 <p className={"pb-2 font-semibold"}>Add an item</p>
                 <input
+                    ref={inputRef}
                     value={itemText}
                     onChange={(event) => {
                         setItemText(event.target.value)
                     }}
                     type="text"
                     className={"w-full rounded-l py-2"}
+                    autoFocus
                 />
                 <Button>Add to item</Button>
             </form>
